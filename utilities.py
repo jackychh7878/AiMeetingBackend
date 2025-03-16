008f8ac1ab27a7af293cbbc4fb9e5f996a229fff
jacky <jackychh7878@gmail.com>
2025-03-16 16:33:08 +0800

    refactor structure

---
+++ utilities.py
@@ -0,0 +1,63 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from pydantic import BaseModel
+import requests
+
+
+# Load environment variables
+load_dotenv()
+
+headers = {
+    "Ocp-Apim-Subscription-Key": os.getenv("AZURE_API_KEY"),  # Replace with your actual key
+    "Content-Type": "application/json"
+}
+
+def check_status(url: str):
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # Raises an error for bad responses
+    json_data = response.json()
+
+    transcribing_status = json_data.get("status", "")
+
+    if transcribing_status == "Succeeded":
+        file_url = json_data.get("links", {}).get("files")  # Avoids unnecessary empty string
+
+        if file_url:
+            content_response = requests.get(file_url, headers=headers)
+            content_json = content_response.json()
+
+            values = content_json.get("values", [])
+            if values:
+                response_url_list = []
+                for item in values:
+                    content_url = item.get("links", {}).get("contentUrl")
+                    response_url_list.append(content_url)
+                return response_url_list
+    else:
+        return "In Progress"
+
+def fetch_completed_transcription(url: str):
+    response = requests.get(url)
+    response.raise_for_status()  # Raises an error for bad responses
+    json_data = response.json()
+
+    speaker_text_pairs = []
+    for phrase in json_data.get("recognizedPhrases", []):
+        speaker = phrase.get("speaker")
+        display_text = phrase.get("nBest", [{}])[0].get("display", "")
+        offset = phrase.get("offsetInTicks", 0) / 10000000  # Convert ticks to seconds
+        duration = phrase.get("durationInTicks", 0) / 10000000  # Convert ticks to seconds
+
+        start_time = format_time(offset)
+        end_time = format_time(offset + duration)
+
+        if speaker is not None and display_text:
+            speaker_text_pairs.append(f"Speaker-{speaker} ({start_time} - {end_time}): {display_text}")
+    return speaker_text_pairs
+
+
+def format_time(seconds):
+    minutes, seconds = divmod(int(seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
