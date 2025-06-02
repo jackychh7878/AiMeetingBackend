64d4ad651069965c9b8a04857fd661d205cfa47b
JackyChong <jacky.chong@catomind.com>
2025-06-02 16:58:55 +0800

    (fix) app remove status code to avoid bug

--- app.py
+++ app.py
@@ -18,33 +18,33 @@ app = Flask(__name__)
 def azure_transcription_api():
     try:
         result = azure_transcription(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 @app.route('/fanolab_submit_transcription', methods=['POST'])
 def fanolab_submit_transcription_api():
     try:
         result = fanolab_submit_transcription(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)})
 
 @app.route('/fanolab_transcription', methods=['POST'])
 def fanolab_transcription_api():
     try:
         result = fanolab_transcription(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
 
 @app.route('/insert_voiceprint', methods=['POST'])
 def insert_voiceprint_api():
     try:
         result = insert_voiceprint(request)
-        return result, 201
+        return result
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
 
 @app.route('/search_voiceprint', methods=['POST'])
 def search_voiceprint_api():
@@ -53,58 +53,57 @@ def search_voiceprint_api():
         if not data or 'path' not in data or 'application_owner' not in data:
             return jsonify({'error': 'Both path and application_owner are required'}), 400
         result = search_voiceprint(data.get('path'), data.get('application_owner'))
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
 
 @app.route('/azure_extract_speaker_clip', methods=['POST'])
 def azure_extract_speaker_clip_api():
     try:
         result = azure_extract_speaker_clip(request)
-
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 @app.route('/azure_match_speaker_voiceprint', methods=['POST'])
 def azure_match_speaker_voiceprint_api():
     try:
         result = azure_match_speaker_voiceprint(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 @app.route('/tflow_get_project_list', methods=['POST'])
 def tflow_get_project_list_api():
     try:
         result = get_project_list(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 @app.route('/tflow_get_project_memory', methods=['POST'])
 def tflow_get_project_memory_api():
     try:
         result = get_project_memory(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 @app.route('/tflow_get_meeting_minutes', methods=['POST'])
 def tflow_get_meeting_minutes_api():
     try:
         result = get_meeting_minutes(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 @app.route('/tflow_get_dashboard', methods=['POST'])
 def tflow_get_dashboard_api():
     try:
         result = get_dashboard(request)
-        return result, 200
+        return result
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)})
 
 
 if __name__ == '__main__':
